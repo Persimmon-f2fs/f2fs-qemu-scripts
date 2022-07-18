3321cee8cf08a82ad9b238b584458646932b8cf0
Samuel Schmidt <sadschmi@ucsc.edu>
2022-07-18 00:50:51 +0000

    updates to execute_workload

--- scripts/execute-workload.sh
+++ scripts/execute-workload.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function usage {
-	echo "Usage: ./execute-workload.sh <output name> <test dir> <device>"
+	echo "Usage: ./execute-workload.sh <output name> <test dir> <device> <fio options>"
 	exit 1
 }
 
@@ -9,39 +9,51 @@ function usage {
 [ -z "$1" ] && usage
 [ -z "$2" ] && usage
 [ -z "$3" ] && usage
+[ -z "$4" ] && usage
 
 sudo mkdir /share/output
 
 output_name=/share/output/$1
 test_dir=$2
 device=$3
+fio_options=$4
 
-echo "==> Testing at $test_dir, with device $device, prefix $output_name"
+blkzone_output=/tmp/${output_name}_blkzone.txt
+meminfo_output=/tmp/${output_name}_meminfo.txt
+beforesmart_output=/tmp/${output_name}_beforesmart.json
+aftersmart_output=/tmp/${output_name}_aftersmart.json
+
+# remove tmp files (potentially from previous run)
+sudo rm $blkzone_output $meminfo_output $beforesmart_output $aftersmart_output
 
 declare -a pids
-sudo perl /share/poll_blkzone.perl $device > ${output_name}_blkzone.txt &
+sudo perl /share/poll_blkzone.perl $device > $blkzone_output &
 pids[0]=$!
-sudo perl /share/poll_meminfo.perl > ${output_name}_meminfo.txt &
+sudo perl /share/poll_meminfo.perl > $meminfo_output &
 pids[1]=$!
 
 # run some stuff
 sudo mkdir $test_dir
 
-echo "==> Recording smart data!"
-sudo smartctl -aj $device > ${output_name}_beforesmart
+sudo smartctl -aj $device > $beforesmart_output
 
-echo "==> Running benchmarks!"
 # sudo filebench -f /share/workloads/fileserver.f > ${output_name}_benchmark
-sudo fio --directory="$test_dir" /share/f2fs-qemu-scripts/fio/garbagecollect.fio
+sudo fio --directory="$test_dir" $fio_options /share/f2fs-qemu-scripts/fio/garbagecollect.fio 1> /dev/null 2>&1
 
-echo "==> Recording smart data!"
-sudo smartctl -aj $device > ${output_name}_aftersmart
+sudo smartctl -aj $device > $aftersmart_output
 
-echo "==> Cleaning up perl scripts!"
 # send interrupt signals
 for pid in ${pids[*]}; do
 	    sudo kill -s SIGINT $pid
 done
 
-echo "==> Done!"
+# copy data from temp files
+echo '{'
+echo '\t"blkzone": $(cat ${blkzone_output}),'
+echo '\t"meminfo": $(cat ${meminfo_output}),'
+echo '\t"beforesmart": $(cat ${beforesmart_output}),'
+echo '\t"aftersmart": $(cat ${aftersmart_output})'
+echo '}'
 
+# cleanup files
+sudo rm $blkzone_output $meminfo_output $beforesmart_output $aftersmart_output
