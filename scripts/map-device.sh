0e718258b46679bd02478441d44eae04d608b466
Samuel Schmidt <sadschmi@ucsc.edu>
2022-09-08 22:44:55 +0000

    added script to prepare devices

--- scripts/map-device.sh
+++ scripts/map-device.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
 usage() {
-	echo "map-device: <size in GB> <device>"
+	echo "map-device: <size in GB> <device> <name of mapped device>"
 	exit 1
 }
 
 [ -z "$1" ] && usage
 [ -z "$2" ] && usage
+[ -z "$3" ] && usage
 
 size=$(($1 * 1024 * 1024 * 1024 / 512))
 device=$2
+mapped_name=$3
 
-echo "Creating a mapping of device: $device of size $size"
+echo "Creating $mapped_name of device: $device of size $size"
 
 # Create an identity mapping for a device
-echo "0 $size linear $device 0" | dmsetup create identity
+if ! echo "0 $size linear $device 0" | dmsetup create $mapped_name; then
+	exit 1
+fi
